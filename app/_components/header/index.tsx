fd835fdcdc81486f85c3e1c7c22ca0227dc2f5ff
SatooRu65536 <87361015+SatooRu65536@users.noreply.github.com>
2024-02-02 21:51:50 +0900

    add: 愛工大マップ

---
+++ app/_components/header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.scss';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>AITガイド</h1>
+    </header>
+  );
+}
