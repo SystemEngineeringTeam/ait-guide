4eb57c40e2d278a0113b87409d1ad2a3030d8617
satooru65536 <ryuto4619@gmail.com>
2024-03-12 13:04:06 +0900

    closed OSMのクレジット表記 #1

--- app/_components/footer/index.tsx
+++ app/_components/footer/index.tsx
@@ -4,6 +4,12 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.copyright}>
+        <a href="https://www.openstreetmap.org/copyright">
+          © OpenStreetMap contributors
+        </a>
+      </p>
+
+      <p className={styles.copyright}>
         {'©SET / created by '}
         <a href="https://www.sysken.net/" target="_blank">
           システム工学研究会
