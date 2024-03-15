400e795fd58e1c4b2eb2d1a70bdd92e775f31bfe
satooru65536 <ryuto4619@gmail.com>
2024-03-15 20:08:47 +0900

    feat: 愛工大外は正門に固定

--- util/convertCoordinates.ts
+++ util/convertCoordinates.ts
@@ -1,4 +1,4 @@
-import { MAX_LAT, MAX_LNG, MIN_LAT, MIN_LNG } from "@/const/map";
+import { MAX_LAT, MAX_LNG, MIN_LAT, MIN_LNG } from '@/const/map';
 
 /**
  * 緯度経度をパーセントに変換する
@@ -7,9 +7,8 @@ import { MAX_LAT, MAX_LNG, MIN_LAT, MIN_LNG } from "@/const/map";
  * @returns [緯度のパーセント, 経度のパーセント]
  */
 export function toPercent(lat: number, lng: number): [number, number] {
-
-  const latPercent = (lat - MIN_LAT) / (MAX_LAT - MIN_LAT) * 100;
-  const lngPercent = (lng - MIN_LNG) / (MAX_LNG - MIN_LNG) * 100;
+  const latPercent = ((lat - MIN_LAT) / (MAX_LAT - MIN_LAT)) * 100;
+  const lngPercent = ((lng - MIN_LNG) / (MAX_LNG - MIN_LNG)) * 100;
 
   return [latPercent, lngPercent];
 }
@@ -18,13 +17,19 @@ export function toPercent(lat: number, lng: number): [number, number] {
  * 座標が範囲外ならnullを返す
  * @param lat 緯度
  * @param lng 経度
- * @returns null or [緯度, 経度]
+ * @returns 正門 or [緯度, 経度]
  */
-export function toValidCoordinate(lat: number, lng: number): [number, number] | undefined {
-  if (lat < MIN_LAT) return undefined;
-  if (lat > MAX_LAT) return undefined;
-  if (lng < MIN_LNG) return undefined;
-  if (lng > MAX_LNG) return undefined;
+export function toValidCoordinate(
+  lat: number,
+  lng: number,
+): [number, number] | undefined {
+  // 正門の緯度経度
+  const mainGate: [number, number] = [35.181396, 137.108965];
+
+  if (lat < MIN_LAT) return mainGate;
+  if (lat > MAX_LAT) return mainGate;
+  if (lng < MIN_LNG) return mainGate;
+  if (lng > MAX_LNG) return mainGate;
 
   return [lat, lng];
 }
