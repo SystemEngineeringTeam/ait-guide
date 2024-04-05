668b63986b184208d689bc92485272272279c5fb
satooru65536 <ryuto4619@gmail.com>
2024-04-05 13:09:54 +0900

    feat: url でシス研を指定できるように

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import AitMap from './_components/aitmap';
 import FacilityList from './_components/facilitylist';
@@ -9,7 +10,18 @@ import styles from './page.module.scss';
 import { INIT_PICKUP, PickUp } from '@/const/pickup';
 
 export default function Home() {
-  const [pickup, setPickup] = useState<PickUp>(INIT_PICKUP);
+  const searchParams = useSearchParams();
+  const sysken = searchParams.get('sysken');
+
+  const SYSKEN_PICKUP: PickUp = {
+    facility: -1,
+    room: 0,
+    secret: 0,
+  };
+
+  const [pickup, setPickup] = useState<PickUp>(
+    sysken === null ? INIT_PICKUP : SYSKEN_PICKUP,
+  );
 
   return (
     <main className={styles.main}>
